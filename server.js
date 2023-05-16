0f0e04ded208baaaa4e9a6129fded4c14cbc3b4c
rjasmin09 <rjasmin2001@gmail.com>
2023-05-15 20:14:28 -0400

    test site

--- server.js
+++ server.js
@@ -23,87 +23,8 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("index");
 });
-app.get("/apply", (req, res) => {
-    res.render("application");
-});
 app.use(bodyParser.urlencoded({extended:false}));
-app.post("/processApplication", async (req, res) => {
-    const uri = `mongodb+srv://${username}:${password}@cluster0.yk8irim.mongodb.net/?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    try {
-        await client.connect();
-        let applicant = {
-            name: req.body.name,
-            email: req.body.email,
-            gpa: req.body.gpa,
-            background: req.body.backgroundInfo
-        };
-        await insertApplicant(client, databaseAndCollection, applicant);
-        res.render("applicationConf", applicant);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-});
-app.get("/reviewApplication", (req, res) => {
-    res.render("reviewApp");
-});
-app.post("/processReviewApplication", async (req, res) => {
-    const uri = `mongodb+srv://${username}:${password}@cluster0.yk8irim.mongodb.net/?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    try {
-        await client.connect();
-        let emailID = req.body.email;
-        let applicant = await lookUpEntry(client, databaseAndCollection, emailID);
-        res.render("applicationConf", applicant);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-});
-app.get("/adminGPA", (req, res) => {
-    res.render("adminGPA");
-});
-app.post("/processAdminGPA", async (req, res) => {
-    const uri = `mongodb+srv://${username}:${password}@cluster0.yk8irim.mongodb.net/?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    try {
-        await client.connect();
-        let gpaVal = req.body.gpa;
-        let applicants = [];
-        applicants = await lookUpGreaterEntries(client, databaseAndCollection, gpaVal);
-        let tableStr = `<table border=1> <tr> <th>Name</th> <th>GPA</th> </tr>`;
-        applicants.forEach(element => {
-            tableStr += `<tr> <td>${element.name}</td> <td>${element.gpa}</td> </tr>`;
-        });
-        tableStr += `</table>`;
-        const variables = { tableGPA: tableStr };
-        res.render("adminGPAConf", variables);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-});
-app.get("/adminRemove", (req, res) => {
-    res.render("adminRemove");
-});
-app.post("/processAdminRemove", async (req, res) => {
-    const uri = `mongodb+srv://${username}:${password}@cluster0.yk8irim.mongodb.net/?retryWrites=true&w=majority`
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    try {
-        await client.connect();
-        let count = await deleteAll(client, databaseAndCollection);
-        const variables = { numRemoved: count };        
-        res.render("adminRemoveConf", variables);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-});
+
 app.use((request, response) => {
     const statusCodeNotFound = 404;
     response.status(statusCodeNotFound).send("Page not found");
@@ -114,22 +35,6 @@ app.listen(portNumber, (err) => {
     } else {
         console.log(`Web server started and running at http://localhost:${portNumber}`);
         process.stdout.write(prompt);
-    }
-});
-
-// command line interpreter
-process.stdin.on('readable', () => {
-	let dataInput = process.stdin.read();
-	if (dataInput !== null) {
-		let command = dataInput.trim();
-		if (command === "stop") {
-			console.log("Shutting down the server");
-            process.exit(0);
-        } else {
-			console.log(`Invalid command: ${command}`);
-		}
-        process.stdout.write(prompt);
-        process.stdin.resume();
     }
 });
 
